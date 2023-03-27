760b49c8b4c41eedc4121582507889d4a08fcfef
wojtekczajka <w.czajka00@gmail.com>
2023-03-27 15:07:24 +0200

    img tag added with similiar functionality like audio and video tag

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -5,10 +5,11 @@ from fastapi.responses import HTMLResponse
 
 # sample video https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4
 # sample audio https://www2.cs.uic.edu/~i101/SoundFiles/StarWars3.wav
+# sample img   https://picsum.photos/200/300
 
 app = FastAPI()
 
-def generate_html_response(video_file: str, audio_file: str):
+def generate_html_response(video_file: str, audio_file: str, img_file: str):
     return f"""
             <html>
                 <head>
@@ -18,10 +19,11 @@ def generate_html_response(video_file: str, audio_file: str):
                 <h1>Hello World Player</h1>
                 {'<h2>Video player</h2><video id="videoPlayer" src="' + video_file + '" controls></video>' if video_file is not None else ''}
                 {'<h2>Audio player</h2><audio id="audioPlayer" src="' + audio_file + '" controls></audio>' if audio_file is not None else ''}
+                {'<h2>Image Poster</h2><img id="posterImage" src="' + img_file + '" controls></audio>' if img_file is not None else ''}
                 </body>
             </html>
     """
 
 @app.get("/")
-async def get_players(videoFile: Union[str, None] = None, audioFile: Union[str, None] = None):
-    return HTMLResponse(content=generate_html_response(videoFile, audioFile))
+async def get_players(videoFile: Union[str, None] = None, audioFile: Union[str, None] = None, imgFile: Union[str, None] = None):
+    return HTMLResponse(content=generate_html_response(videoFile, audioFile, imgFile))
