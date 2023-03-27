70e56e97e26612257b77c33262e9a5518b92c302
wojtekczajka <w.czajka00@gmail.com>
2023-03-27 14:50:03 +0200

    added html video tag and audio tag to html response

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -3,6 +3,9 @@ from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
+# sample video https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4
+# sample audio https://www2.cs.uic.edu/~i101/SoundFiles/StarWars3.wav
+
 app = FastAPI()
 
 def generate_html_response(video_file: str, audio_file: str):
@@ -13,6 +16,8 @@ def generate_html_response(video_file: str, audio_file: str):
                 </head>
                 <body>
                 <h1>Hello World Player</h1>
+                {'<h2>Video player</h2><video id="videoPlayer" src="' + video_file + '" controls></video>' if video_file is not None else ''}
+                {'<h2>Audio player</h2><audio id="audioPlayer" src="' + audio_file + '" controls></audio>' if audio_file is not None else ''}
                 </body>
             </html>
     """
