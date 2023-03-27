222abb0d978716621131f0dfb66aa0176de770cc
wojtekczajka <w.czajka00@gmail.com>
2023-03-27 14:41:19 +0200

    added HTMLResponse and handling videoFile and audioFile query args(optionall)

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+def generate_html_response(video_file: str, audio_file: str):
+    return f"""
+            <html>
+                <head>
+                    <title>Hello World Player</title>
+                </head>
+                <body>
+                <h1>Hello World Player</h1>
+                </body>
+            </html>
+    """
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def get_players(videoFile: Union[str, None] = None, audioFile: Union[str, None] = None):
+    return HTMLResponse(content=generate_html_response(videoFile, audioFile))
