11666c8693312b8449019cc7cfb21cf906143021
wojtekczajka <w.czajka00@gmail.com>
2023-04-17 14:24:03 +0200

    update

---
+++ lab3/source/server.py
@@ -0,0 +1,58 @@
+from typing import Union
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+def generate_video_player(video_file: str):
+    if video_file is None:
+        return ""
+    return f"""
+        <h2>Video player</h2>
+        <video id="videoPlayer" src="{video_file}" controls></video>
+        <button id="videoCancel" onclick="document.getElementById('videoPlayer').src='cancel.mp4'">Cancel Video</button>
+    """
+
+def generate_audio_player(audio_file: str):
+    if audio_file is None:
+        return ""
+    return f"""
+        <h2>Audio player</h2>
+        <audio id="audioPlayer" src="{audio_file}" controls></audio>
+        <button id="audioCancel" onclick="document.getElementById('audioPlayer').src='cancel.mp3'">Cancel Audio</button>
+    """
+
+def generate_image_poster(img_file: str):
+    if img_file is None:
+        return ""
+    return f"""
+            <h2>Image Poster</h2>
+            <img id="posterImage" src="{img_file}" controls>
+    """
+
+def generate_html_response(video_file: str, audio_file: str, img_file: str):
+    return f"""
+        <html>
+            <head>
+                <title>Hello World Player</title>
+            </head>
+            <body>
+                <h1>Hello World Player</h1>
+                {generate_video_player(video_file)}
+                {generate_audio_player(audio_file)}
+                {generate_image_poster(img_file)}
+            </body>
+        </html>
+    """
+
+@app.get("/")
+async def get_players(videoFile: Union[str, None] = None, audioFile: Union[str, None] = None, imgFile: Union[str, None] = None):
+    return HTMLResponse(content=generate_html_response(videoFile, audioFile, imgFile))
+
+# sample video https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4
+# sample audio https://www2.cs.uic.edu/~i101/SoundFiles/StarWars3.wav
+# sample img   https://picsum.photos/200/300
+
+# request url http://127.0.0.1:4080/?videoFile=https%3A%2F%2Fsample-videos.com%2Fvideo123%2Fmp4%2F720%2Fbig_buck_bunny_720p_1mb.mp4&audioFile=https%3A%2F%2Fwww2.cs.uic.edu%2F~i101%2FSoundFiles%2FStarWars3.wav&imgFile=https%3A%2F%2Fpicsum.photos%2F200%2F300
